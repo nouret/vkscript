290e5cab5cba85bfda5f3985a53c475f9a075119
nouret <rfnourettuz@gmail.com>
2018-04-11 11:51:01 +0300

    err changes

---
+++ main2_akop.py
@@ -0,0 +1,89 @@
+from __future__ import unicode_literals
+import pprint
+from urllib.parse import parse_qs
+import webbrowser
+import pickle
+from datetime import datetime, timedelta
+import time
+import requests
+from random import randint, choice
+
+import vk
+
+
+def get_auth_params():
+    auth_url = ("https://oauth.vk.com/authorize?client_id={app_id}"
+                "&scope=groups&revoke=1&redirect_uri=http://oauth.vk.com/blank.html"
+                "&display=page&response_type=token".format(app_id=6068794))
+    print(auth_url)
+    """webbrowser.open_new_tab(auth_url)
+    redirected_url = input("Paste here url you were redirected:\n")
+    aup = parse_qs(redirected_url)
+    aup['access_token'] = aup.pop(
+        'https://oauth.vk.com/blank.html#access_token')
+    save_auth_params(aup['access_token'][0], aup['expires_in'][0],
+                     aup['user_id'][0])
+    return aup['access_token'][0], aup['user_id'][0]
+    """
+
+AT = "3f3a9551cc3a357ba1d04f0880db95f60c305f8926fc0d1873512d001df15e312bac5290bc3982da64a9a"
+#session = vk.Session()
+session = vk.Session(access_token = AT)
+api = vk.API(session)
+#print(api.users.get(user_ids=29629954))
+#print(api.users.getinfo(user_ids=29629954))
+#print(api.wall.post(massage = "Hello world!"))
+#print(api.wall.get(domain='nouret',count=1))
+#https://api.vk.com/method/board.getComments
+#get_auth_params() #for new
+#exit(0)
+#print(len(All["comments"]))
+capcha_sleep = 1
+captcha_key = -1
+false_key = False
+while True:
+    time.sleep(0.1)
+    if randint(100, 200) == 179:
+        print("I am working")
+    try:
+        if captcha_key != -1:
+            First = api.board.getComments(group_id='30666517', topic_id='25736069', count=1, offset=0, captcha_key=captcha_key, captcha_sid=captcha_sid)
+            captcha_key = -1
+            print("good captcha")
+            false_key = False
+        else:
+            First = api.board.getComments(group_id='30666517', topic_id='25736069', count=1, offset=0)
+        total_comments = First["comments"][0]
+        Last = api.board.getComments(group_id='30666517', topic_id='25736069', count=1, offset=total_comments - 1)
+        can = Last["comments"][1]["from_id"] != 458578456
+        if can:
+            capcha_sleep = 1
+            api.board.createComment(access_token=AT, group_id='30666517', topic_id='25736069', message="0x0" + hex(total_comments)[2:])
+            print("new comment", "0x0" + hex(total_comments)[2:], total_comments / (2**20), "I am akop")
+    except requests.exceptions.ReadTimeout:
+        print("requests.exceptions.ReadTimeout")
+        time.sleep(2)
+    except IndexError:
+        print("IndexError")
+        time.sleep(2)
+    except vk.exceptions.VkAPIError as error:
+        if error.is_captcha_needed():
+            if not false_key:
+                false_key = True
+                captcha_sid = error.captcha_sid
+                captcha_key = [choice("qwertyuiopasdfghjklzxcvbnm1234567890") for _ in range(5)]
+                time.sleep(10)
+            else:
+                false_key = False
+                print("captcha_imgcha")
+                print(error.captcha_img)
+                auth_url = error.captcha_img
+                webbrowser.open_new_tab(auth_url)
+                captcha_sid = error.captcha_sid
+                time.sleep(5)
+                captcha_key = input("enter captcha ").rstrip()
+                print(captcha_key)
+                time.sleep(5)
+        time.sleep(capcha_sleep)
+        if capcha_sleep < 20:
+            capcha_sleep += 1
---
+++ main_openCV.cpp
@@ -0,0 +1,168 @@
+﻿#include "opencv2/opencv.hpp"
+#include "opencv/cv.hpp"
+#include "opencv/cv.h"
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+
+vector<pair<int, int>> BFS(int x, int y);
+
+Mat img;
+int lost;
+double tmp;
+vector<vector<int>> F;
+
+int main(int argc, const char** argv)
+{
+	img = imread("1.jpg", CV_LOAD_IMAGE_COLOR);
+	int r = img.rows;
+	int c = img.cols;
+	F.resize(r);
+	vector<pair<int, int>> Plus;
+	vector<pair<int, int>> Minus;
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+		{
+			F[i].push_back(0);
+		}
+	}
+	lost = r * c;
+	int MaX;
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+		{
+			int sum = (img.at<Vec3b>(i, j)[0] - 255) ^ 2 + (img.at<Vec3b>(i, j)[1] - 255) ^ 2 + (img.at<Vec3b>(i, j)[2] - 255) ^ 2;
+			MaX = max(MaX, sum);
+			printf("%d\n", MaX);
+		}
+	}
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+		{
+			int sum = (img.at<Vec3b>(i, j)[0] - 255) ^ 2 + (img.at<Vec3b>(i, j)[1] - 255) ^ 2 + (img.at<Vec3b>(i, j)[2] - 255) ^ 2;
+			if (sum < MaX / 3) // white
+			{
+				for (int k = 0; k < 3; ++k)
+				{
+					img.at<Vec3b>(i, j)[k] = 255;
+				}
+			}
+			else // black
+			{
+				for (int k = 0; k < 3; ++k)
+				{
+					img.at<Vec3b>(i, j)[k] = 0;
+				}
+			}
+			if ((min(i, r - i) / r + min(j, c - j) / c) < 1 / 3) // corners to white
+			{
+				for (int k = 0; k < 3; ++k)
+				{
+					img.at<Vec3b>(i, j)[k] = 255;
+				}
+			}
+		}
+	}
+
+	/*for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+		{
+			if (F[i][j] == 0)
+			{
+
+				vector<pair<int, int>> part = BFS(i, j);
+				int partmax0 = part[0].first;
+				int partmin0 = part[0].first;
+				int partmax1 = part[0].second;
+				int partmin1 = part[0].second;
+				for (int l = 0; l < part.size(); ++l)
+				{
+
+				}
+				int dy = max([elem[0] for elem in part]) - min([elem[0] for elem in part]);
+				int dx = max([elem[1] for elem in part]) - min([elem[1] for elem in part]);
+				int y = max([elem[0] for elem in part]) + min([elem[0] for elem in part]);
+				int x = max([elem[1] for elem in part]) + min([elem[1] for elem in part]);
+				if 2 * dx >= dy and dx < 2 * dy and 8 * (dx + dy) > len(part) and len(part) > 20:
+	Plus += [(y // 2, x // 2)]
+		for elem in part :
+	img[elem[0]][elem[1]][0] = 0
+		img[elem[0]][elem[1]][1] = 0
+		img[elem[0]][elem[1]][2] = 255
+		elif dx >= 2 * dy and 4 * (dx + dy) > len(part) and len(part) > 10:
+	Minus += [(y // 2, x // 2)]
+		for elem in part :
+	img[elem[0]][elem[1]][0] = 255
+		img[elem[0]][elem[1]][1] = 0
+		img[elem[0]][elem[1]][2] = 0
+		elif len(part) < 500 :
+		for elem in part :
+	img[elem[0]][elem[1]][0] = 0
+		img[elem[0]][elem[1]][1] = 255
+		img[elem[0]][elem[1]][2] = 0
+		print("+:", len(Plus))
+		print("-:", len(Minus))
+		*/
+	namedWindow("MYWINDOW", WINDOW_NORMAL);
+	imshow("MYWINDOW", img);
+
+	/*printf("[i] channels:  %d\n", image->nChannels);
+	printf("[i] pixel depth: %d bits\n", image->depth);
+	printf("[i] width:       %d pixels\n", image->width);
+	printf("[i] height:      %d pixels\n", image->height);
+	printf("[i] image size:  %d bytes\n", image->imageSize);
+	printf("[i] width step:  %d bytes\n", image->widthStep);*/
+
+	waitKey(0);
+
+	return 0;
+}
+
+
+
+
+vector<pair<int, int>> BFS(int x, int y)
+{
+	vector<pair<int, int>> ans;
+	F[x][y] = 1;
+	queue<pair<int, int>> Q;
+	pair<int, int> p;
+	p.first = x;
+	p.second = y;
+	Q.push(p);
+	while (!Q.empty()) {
+		p = Q.front();
+		lost -= 1;
+		printf("%d \n", lost);
+		ans.push_back(p);
+		for (int dx = -4; dx <= 4; ++dx) {
+			for (int dy = -4; dy <= 4; ++dx) {
+				if ((dx != 0) || (dy != 0)) {
+					int nx = x + dx;
+					int ny = y + dy;
+					pair<int, int> np(nx, ny);
+					bool eq = true;
+					for (int k = 0; k < 3; ++k) {
+						if (img.at<Vec3b>(nx, ny)[k] != img.at<Vec3b>(x, y)[k])
+						{
+							bool eq = false;
+						}
+					}
+					if ((0 <= nx) && (nx < img.cols) && ((0 <= ny) && (ny < img.rows)) && (eq) && (!F[nx][ny])) {
+						Q.push((np));
+						F[nx][ny] = 1;
+					}
+				}
+			}
+		}
+	}
+					return ans;
+}
